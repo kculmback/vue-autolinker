0b73fc5470d89a230911466df40a13b7f75c4187
Kasey Culmback <kasey@14four.com>
2019-11-13 17:09:09 -0800

    add examples

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p>
+      For more information about configuration, check out
+      <a href="https://github.com/gregjacobs/Autolinker.js">Autolinker.js</a>.
+    </p>
+    <p>
+      See vue-autolinker documentation
+      <a href="https://github.com/kculmback/vue-autolinker">here</a>.
+    </p>
+    <div class="examples">
+      <p v-autolinker="exampleOne"></p>
+      <p v-autolinker:[options]="exampleTwo"></p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
-};
+  name: 'app',
+  data() {
+    return {
+      exampleOne:
+        'Text with a link (https://github.com/gregjacobs/Autolinker.js).',
+      exampleTwo:
+        'Text with a link (vuejs.com), a mention (@vuejs), and a hashtag (#vue).',
+      options: {
+        mention: 'twitter',
+        hashtag: 'twitter',
+      },
+    }
+  },
+}
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.examples {
+  display: inline-block;
+  margin: 2rem auto;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+  padding: 1rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App.vue'
+import autolinker, { AUTOLINKER_NAME } from './directives/autolinker'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+Vue.directive(AUTOLINKER_NAME, autolinker)
 
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
